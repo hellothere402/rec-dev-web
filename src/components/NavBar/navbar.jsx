3ba33907c8d37128743489ad3525d30657dfb73b
hellothere402 <archiejohnbruce@outlook.com>
2023-05-27 20:25:53 +1200

    CSS and NavBar updates

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {RiMenu3Line,RiCloseLine} from 'react-icons/ri';
 import './navbar.css'
+import logo from '../../assets/logo.png'
+
+// BEM - Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>navbar</div>  )
+    <div className="Recycling__navbar">
+      <div className="Recycling__navbar-links">
+        <div className="Recycling__navbar-links-logo">
+          <img src={logo} alt="logo" />
+          </div>
+       </div>
+    </div>  
+  )
 }
 
 export default Navbar
