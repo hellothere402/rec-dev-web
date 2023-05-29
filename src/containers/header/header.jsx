dab0ab90f35e01e4c0f3c1185f241852067674c3
hellothere402 <archiejohnbruce@outlook.com>
2023-05-29 14:45:50 +1200

    Updated the Header of the Website, And made the final changes to the NavBar.

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
 
+
 const Header = () => {
   return (
-    <div>H</div>
+    <div className="recycling__header section__padding" id="home">
+      <div className="recycling__header-content">
+        <h1 className="gradient__text">Join the movement. Do your part for the school, and the enviroment</h1>
+        <p>Join the Movement, do your part for the enviroment and the school. In helping clean up the rubbish around Dunstan High School. </p>
+
+        <div className="recycling__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Join the Movement</button>
+        </div>
+
+        <div className="recycling__header-content__people">
+            // img of people //
+        </div>
+        <div className="recycling__header-img">
+          // img of recycling //
+        </div>
+      </div>
+    </div>
   )
 }
 
